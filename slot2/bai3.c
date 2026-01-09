09fc5842a355cbf3bba5d691d6f8a4121bb819ce
Hung <karikahami@gmail.com>
2026-01-09 11:24:13 +0700

    bai3

---
+++ slot2/bai3.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main (){
+	float a = 5;
+	float b = 10.216 ;
+	float sum = a + b ;
+	printf ("a= %1.0f \n", a );
+	printf ("b= %3.3f \n", b) ;
+	printf ("ket qua = %3.3f ", sum) ;
+	return 0;
+}
