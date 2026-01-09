a4d66c095730cadddadacee52dc9ee5568d196ac
Hung <karikahami@gmail.com>
2026-01-09 10:32:14 +0700

    tai bai 1 len

---
+++ slot2/bai1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main (){
+	//khao bao 1 so nguyen
+	int x ;
+	//khoi tao gia tri bat ky
+	x = 10;
+	//In ra man hinh
+	printf("In ra man hinh gia tri ne: %d",x);
+	return 0;
+}
