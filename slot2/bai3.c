9b544a4f4aad9297c67f0762b6033912e57a82a9
Hung <karikahami@gmail.com>
2026-01-09 11:43:29 +0700

    bai3slot2

--- slot2/bai3.c
+++ slot2/bai3.c
@@ -2,9 +2,9 @@
 int main (){
 	float a = 5;
 	float b = 10.216 ;
-	float sum = a + b ;
+	float kq = a + b ;
 	printf ("a = %0.0f \n", a );
 	printf ("b = %0.3f \n", b) ;
-	printf ("%.0f + %0.3f = %0.2f ", a,b,sum ) ;
+	printf ("%.0f + %0.3f = %0.2f ", a,b,kq) ;
 	return 0;
 }
