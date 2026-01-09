c785d05102bd3e3919c9a94f7b7f8503cda3052f
Hung <karikahami@gmail.com>
2026-01-09 10:49:27 +0700

    hoanthienbai1

--- slot2/bai1.c
+++ slot2/bai1.c
@@ -1,10 +1,28 @@
 #include <stdio.h>
-int main (){
-	//khao bao 1 so nguyen
-	int x ;
-	//khoi tao gia tri bat ky
-	x = 10;
-	//In ra man hinh
-	printf("In ra man hinh gia tri ne: %d",x);
-	return 0;
+int main () {
+    // khai bao 1 so nguyen
+    int x;
+    // Giai thich: Khai bao kieu gia tri cho bien x, o day la so nguyen (int)
+
+    // khoi tao gia tri bat ky
+    x = 10;
+    // Giai thich: Gan gia tri cho bien x
+
+    // In ra man hinh
+    printf("In ra man hinh gia tri ne: %d \n", x);
+    // Giai thich: in ra man hinh qua ham printf,
+    // %d la chuoi dinh dang (format string),
+    // %d o day dai dien cho so nguyen,
+    // \n la cu phap xuong dong
+
+    /*
+    Giai thich them :
+    int main() co nghia la chuong trinh duoc bat dau tu day;
+    int la chuong trinh tra ve mot so nguyen cho he dieu hanh;
+    {} la mot khoi lenh;
+    ";" hoat dong tuong tu nhu dau cham het cau, dung de ket thuc cau
+    return 0; 
+    */
+
+    return 0;
 }
