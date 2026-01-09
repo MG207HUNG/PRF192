7d7116acc29704fcf3eee81768731ae0a8f96634
Hung <karikahami@gmail.com>
2026-01-09 10:07:33 +0700

    add slot1

---
+++ slot1/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+	int main (){
+		char name[] = "Hung";
+		int age	= 18;
+		double grade = 9.8;
+		printf("Hello xin chao ban la :%s \n" , name);
+		printf("Tuoi cua ban la : %d \n" , age);
+		printf("Diem mong muon cua ban la %f \n", grade);
+		return 0;
+		// day la binh luan trong 1 dong
+		/* va day la binh luan co the xuong dong
+		vi du la nhu nay */
+	}
+	
