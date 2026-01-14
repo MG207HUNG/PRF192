7922fe3b767e8136ec1e63453511ab041ee001ea
Hung <karikahami@gmail.com>
2026-01-14 09:15:58 +0700

    Baitapslot3

--- slot3/ex2.c
+++ slot3/ex2.c
@@ -8,8 +8,11 @@ kich thuoc kieu
 
 */
 int main(){
-	char kt = 'C';
-	printf("%-16s %-16s %-16s %-16s %-16s %s\n" ,"Ten Bien" , "Gia tri" , "Dec" , "Hex" , "Oct" , "Size");
-	printf("%-16s %-16c %-16d %-16x %-16o %-16d" , "kt" , kt , kt , kt , kt , sizeof(kt));
-	return 0;
+	char kt1 = 'C' ,kt2 = '0' , kt3 = 'Z' , kt4 ='B' , kt5 ='i';
+	printf("%-16s|%-16s|%-16s|%-16s|%-16s|%s\n" ,"Ten Bien" , "Gia tri" , "Dec" , "Hex" , "Oct" , "Size");
+	printf("%-16s|%-16c|%-16d|%-16x|%-16o|%-16d\n" , "kt1" , kt1 , kt1 , kt1 , kt1 , sizeof(kt1));
+	printf("%-16s|%-16c|%-16d|%-16x|%-16o|%-16d\n" , "kt2" , kt2 , kt2 , kt2 , kt2 , sizeof(kt2));
+	printf("%-16s|%-16c|%-16d|%-16x|%-16o|%-16d\n" , "kt3" , kt3 , kt3 , kt3 , kt3 , sizeof(kt3));
+	printf("%-16s|%-16c|%-16d|%-16x|%-16o|%-16d\n" , "kt4" , kt4 , kt4 , kt4 , kt4 , sizeof(kt4));
+	printf("%-16s|%-16c|%-16d|%-16x|%-16o|%-16d\n" , "kt5" , kt5 , kt5 , kt5 , kt5 , sizeof(kt5));
 }
