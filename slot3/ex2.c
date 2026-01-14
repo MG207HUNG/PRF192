64a11496bd09af7faf696450f5175ef96728f574
Hung <karikahami@gmail.com>
2026-01-14 08:30:35 +0700

    baitap2Slot3

---
+++ slot3/ex2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/*2. Khai bao kieu ki tu ten kt , khoi gan gia tri cho bien nay
+In ra : gia tri cua bien :
+Gia tri thap phan , gia tri hex , gia tri octal
+kich thuoc kieu 
+
+
+
+*/
+int main(){
+	char kt = 'a';
+	printf("Gia tri cua bien : %c \n" , kt );
+	printf("Gia tri thap phan : %d \n" , kt );
+	printf("Gia tri hex : %x \n" , kt );
+	printf("Gia tri octal : %o \n" , kt );
+	
+	
+	return 0;
+}
