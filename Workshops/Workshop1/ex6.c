74ff9eaea1b5d5d78573231ed70d173d8c1b5a7a
Hung <karikahami@gmail.com>
2026-01-23 12:07:12 +0700

    workshop1

--- Workshops/Workshop1/ex6.c
+++ Workshops/Workshop1/ex6.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
-#include <math.h>
 int main (){
 	float cC , gK , cK ,tD ;
 	char chuCai ;
 	printf("Diem chuyen can :");
-	if(scanf(" %f", &cC)!=1 || cC <=0){
+	if(scanf(" %f", &cC)!=1 || cC <0 || cC > 10){
 		printf("Nhap sai");
 		return ;
 	}
 	printf("Diem giua ky :");
-	if(scanf(" %f", &gK)!=1 || gK <=0){
+	if(scanf(" %f", &gK)!=1 || gK <0 || gK > 10){
 		printf("Nhap sai");
 		return ;
 	}
 	printf("Diem cuoi ky :");
-	if(scanf(" %f", &cK)!=1 || cK <=0){
+	if(scanf(" %f", &cK)!=1 || cK <0 || cK > 10){
 		printf("Nhap sai");
 		return ;
 	}
@@ -22,18 +21,18 @@ int main (){
 	if (tD<4){
 		chuCai = 'F';
 		} 
-	else if (tD>= 4 && tD<=5.4){
+	else if (tD<=5.4){
 		chuCai = 'D';
 
 	}
-	else if (tD>= 5.5 && tD<=6.9){
+	else if (tD<=6.9){
 		chuCai = 'C';
 
 	}
-	else if (tD>= 7 && tD<= 8.4){
+	else if (tD<= 8.4){
 		chuCai = 'B';
 	}
-	else if (tD>= 8.5 && tD<= 10){
+	else if (tD<= 10){
 		chuCai = 'A';
 	}
 	if ( cC >=4 && gK >=4 && cK >= 4 && chuCai != 'F' ){
