096031c19d10368b2c9053977d87606532651d40
Hung <karikahami@gmail.com>
2026-01-23 11:30:59 +0700

    workshop1

--- Workshops/Workshop1/ex5.c
+++ Workshops/Workshop1/ex5.c
@@ -24,7 +24,7 @@ int main(){
 				c = 12000 + 15000*29.5 + (s-30)*12000;
 			}
 	if(t>5){
-	c = c + 1000*(t-5) ;
+	c += 1000*(t-5) ;
 }
 	printf ("%-20s %-20s %-20s \n", "So Km", "So phut cho" , "Tong tien");
 	printf ("%-20.1f %-20d %-20.0f", s , t , c);
