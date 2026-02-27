08bee8b12a2244a142b56dfdd95e94247da7d145
Hung <karikahami@gmail.com>
2026-02-27 11:34:50 +0700

    slot12

--- slot12/ex1.c
+++ slot12/ex1.c
@@ -1,31 +1,25 @@
 #include <stdio.h>
 int sumOfDivisors(int n){
 	int sum = 0 ;
-	for(int i = 1 ; i <= n ; i++){
+	for(int i = 1 ; i < n/2 ; i++){
 		if(n%i==0){
 			sum=sum+i;
 		}
 	}
 	return sum;
 }
-int maxOf3(int a , int b , int c){
-	return ( a>b?(a>c?a:c):(b>c?b:c));
-}
-void printASCII(){
-	for(char c = 'A'; c<='Z';c++){
-		printf("%c : %d\n",c,c);
+int getInput(){
+	int soInt;
+	printf("Nhap so int : " );
+	while(scanf("%d",&soInt)!=1 || soInt <= 0){
+		printf("Nhap sai!\nNhap lai:");
+		fflush(stdin);
+	return soInt;
 	}
 }
 int main(){
-	int n ;
-	int a,b,c;
-	printf("Nhap n :");
-	scanf("%d",&n);
+	int n;
+	n = getInput();
 	printf("Tong cac uoc so cua n la :%d \n",sumOfDivisors(n));
-	printf("Nhap 3 so :");
-	scanf("%d,%d,%d",a,b,c);
-	printf("So lon nhat trong 3 so la :%d \n",maxOf3(a,b,c));
-	printf("Ma ASCII cua cac ki tu tu A->Z la : " );
-	printASCII();
 	return 0;
 }
